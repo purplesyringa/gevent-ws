f5e5aa1193ef8f4251a3679f9871800383cc71a5
Ivanq <imachug@yandex.ru>
2020-02-25 12:22:47 +0300

    Next version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gevent-ws",
-    version="1.0.0",
+    version="1.0.1",
     description="Websocket server for gevent.pywsgi",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
