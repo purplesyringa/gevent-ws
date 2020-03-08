e8778766e5e3e9d87a72186e7e93aa85c6d2f485
Ivanq <imachug@yandex.ru>
2020-03-08 23:51:13 +0300

    Fix case-sensitivity, v2.0.0

--- gevent_ws/__init__.py
+++ gevent_ws/__init__.py
@@ -211,7 +211,7 @@ class WebSocketHandler(WSGIHandler):
 
 
         http_connection = [s.strip() for s in self.environ.get("HTTP_CONNECTION", "").split(",")]
-        if "Upgrade" not in http_connection or self.environ.get("HTTP_UPGRADE", "") != "websocket":
+        if "upgrade" not in http_connection.lower() or self.environ.get("HTTP_UPGRADE", "") != "websocket":
             # Not my problem
             return super(WebSocketHandler, self).handle_one_response()
 
