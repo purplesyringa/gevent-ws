450fac5cb5a9992c3eab30a2a91ed28d6a284214
Ivanq <imachug@yandex.ru>
2020-05-11 20:31:40 +0300

    v2.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gevent-ws",
-    version="2.0.8",
+    version="2.1.0",
     description="Websocket server for gevent.pywsgi",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
